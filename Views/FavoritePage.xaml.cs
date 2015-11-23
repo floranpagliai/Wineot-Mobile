1239bd4bafeb2db64be113c8e6826d3b8d436170
Floran Pagliai <floran.pagliai@gmail.com>
2015-11-23 23:48:25 +0100

    #6 Created favorite page and get data from serveur

---
+++ Views/FavoritePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace Wineot
+{
+	public partial class FavoritePage : ContentPage
+	{
+		ObservableCollection<WineModel> _wines = new ObservableCollection<WineModel>();
+
+		public FavoritePage ()
+		{
+			this.Icon = "unlike.png";
+			Title = "Favoris"; 
+			InitializeComponent ();
+		}
+	
+		async void GetWineHistoryAction()
+		{
+			var wines = UserService.getInstance.getUser ().favoriteWines;
+			foreach(var wine in wines)
+			{
+				WineModel wineElem = await WineService.getInstance.GetWineAction (wine);
+
+				if (wineElem.name != "")
+					_wines.Add (wineElem);
+			}
+		}
+	}
+}
+
--- Views/MenuPage.xaml.cs
+++ Views/MenuPage.xaml.cs
@@ -12,6 +12,7 @@ namespace Wineot
 			this.Title = "Wine'ot";
 			this.Children.Add(new CameraPage());
 			this.Children.Add(new HistoryPage());
+			this.Children.Add(new FavoritePage());
 			this.HeightRequest = 30;
 			InitializeComponent ();
 		}
